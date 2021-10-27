f147f7b1038caba032a185ba79a6e762c5628e65
sotrnguy92 <sonnguyen092@gmail.com>
2021-10-26 21:21:21 -0700

    resetting for practice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import {useWeatherData} from "./hooks/useWeatherData";
+
 
 
 function App() {
-  const weatherData = useWeatherData('San Jose')
-  console.log(weatherData)
-
   return (
+
     <div className="App">
 
     </div>
