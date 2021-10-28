01d07d1a29fe36052c11b5512dac506c91fdd8bb
sotrnguy92 <sonnguyen092@gmail.com>
2021-10-28 12:41:17 -0700

    resetting for practice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import {useWeatherData}  from './hooks/useWeatherData'
-import {SingleDayComponent} from "./components/SingleDayComponent";
+
 
 
 function App() {
-  const data = useWeatherData("San Jose")
   return (
 
     <div className="App">
-      {data.length > 0 && <SingleDayComponent data={data[0]}/>}
     </div>
   );
 }
