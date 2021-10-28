e2a9feb62e596c87387597e2305ae4e9aa029d81
sotrnguy92 <sonnguyen092@gmail.com>
2021-10-28 13:02:14 -0700

    passing min, max, icon, and dayName from api to App container

--- src/hooks/useWeatherData.js
+++ src/hooks/useWeatherData.js
@@ -3,18 +3,25 @@ import axios from 'axios';
 
 
 export const useWeatherData =  (city) => {
-    const [data, setData] = useState([]);
+    const [weatherArr, setWeatherArr] = useState([]);
     const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=Imperial&appid=2a23152aca4c1c4bf50bf677554dedf4`;
     useEffect(() => {
         const weatherData = async () =>{
             const res = await axios.get(url)
 
             const data = res.data
-
             console.log(data)
-            setData(data.list)
+
+           const fiveDayWeather = data.list.filter((_,index) => index%8 === 0).map(day => {
+               const min = day.main.temp_min;
+               const max = day.main.temp_max;
+               const iconData = {iconImageId: day.weather[0].icon, iconDescription: day.weather[0].description}
+               const dayName = new Date(day.dt_txt).toLocaleDateString(undefined, {weekday: "short"});
+               return {min, max, iconData, dayName}
+           })
+            setWeatherArr(fiveDayWeather)
         }
         weatherData()
     }, [url])
-    return data;
+    return weatherArr;
 }
